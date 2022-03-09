9934b5307fbcf8e36115dba3929faf5b457a7c4e
swwind <swwind233@gmail.com>
2022-03-09 14:58:23 +0800

    feat: switch to pending ui

--- app/routes/problemset/$sid/edit.tsx
+++ app/routes/problemset/$sid/edit.tsx
@@ -1,13 +1,11 @@
 import { Problem, ProblemSet, ProblemSetTag } from "@prisma/client";
 import {
-  ActionFunction,
-  Form,
   json,
-  LoaderFunction,
+  useFetcher,
   MetaFunction,
-  redirect,
   useLoaderData,
-  useTransition,
+  ActionFunction,
+  LoaderFunction,
 } from "remix";
 import { db } from "~/utils/db.server";
 import { ensureId, invariant } from "~/utils/invariant";
@@ -15,7 +13,7 @@ import { ensureId, invariant } from "~/utils/invariant";
 type LoaderData = {
   problemSet: ProblemSet & {
     tags: ProblemSetTag[];
-    problems: Pick<Problem, "pid">[];
+    problems: Pick<Problem, "pid" | "title">[];
   };
 };
 
@@ -27,104 +25,278 @@ export const loader: LoaderFunction = async ({ params }) => {
     include: { tags: true, problems: { select: { pid: true, title: true } } },
   });
 
+  if (!problemSet) {
+    throw new Response("Problem Set not found", { status: 404 });
+  }
+
   return json({ problemSet });
 };
+
+enum ActionType {
+  CreateTag = "createTag",
+  DeleteTag = "deleteTag",
+  CreateProblem = "createProblem",
+  DeleteProblem = "deleteProblem",
+  UpdateInfomation = "updateInfomation",
+}
 
 export const action: ActionFunction = async ({ params, request }) => {
   const sid = invariant(ensureId(params.sid), "sid is required");
   const form = await request.formData();
 
-  const title = invariant(form.get("title")?.toString(), "title is required");
-  const description = invariant(
-    form.get("description")?.toString(),
-    "description is required"
-  );
-  const tags = (form.get("tags")?.toString() || "")
-    .split(",")
-    .map((a) => a.trim())
-    .filter(Boolean);
-  const problems = (form.get("problems")?.toString() || "")
-    .split(",")
-    .map((a) => a.trim())
-    .filter(Boolean)
-    .map((a) => invariant(ensureId(a), "pid should be valid"));
+  const _action = form.get("_action");
 
-  await db.problemSetTag.createMany({
-    data: tags.map((tag) => ({
-      name: tag,
-    })),
-    skipDuplicates: true,
-  });
+  switch (_action) {
+    case ActionType.CreateProblem: {
+      const pid = invariant(
+        ensureId(form.get("pid")?.toString()),
+        "pid is required"
+      );
 
-  await db.problemSet.update({
-    where: { sid },
-    data: {
-      title,
-      description,
-      tags: {
-        set: tags.map((tag) => ({ name: tag })),
-      },
-      problems: {
-        connect: problems.map((pid) => ({ pid })),
-      },
-    },
-  });
+      await db.problemSet.update({
+        where: { sid },
+        data: {
+          problems: {
+            connect: {
+              pid,
+            },
+          },
+        },
+      });
 
-  return redirect(`/problemset/${sid}`);
+      return null;
+    }
+
+    case ActionType.DeleteProblem: {
+      const pid = invariant(
+        ensureId(form.get("pid")?.toString()),
+        "pid is required"
+      );
+
+      await db.problemSet.update({
+        where: { sid },
+        data: {
+          problems: {
+            disconnect: {
+              pid,
+            },
+          },
+        },
+      });
+
+      return null;
+    }
+
+    case ActionType.CreateTag: {
+      const tag = invariant(form.get("tag")?.toString(), "Tag is required");
+
+      await db.problemSet.update({
+        where: { sid },
+        data: {
+          tags: {
+            connectOrCreate: {
+              where: { name: tag },
+              create: { name: tag },
+            },
+          },
+        },
+      });
+
+      return null;
+    }
+
+    case ActionType.DeleteTag: {
+      const tag = invariant(form.get("tag")?.toString(), "Tag is required");
+
+      await db.problemSet.update({
+        where: { sid },
+        data: {
+          tags: {
+            disconnect: {
+              name: tag,
+            },
+          },
+        },
+      });
+
+      return null;
+    }
+
+    case ActionType.UpdateInfomation: {
+      const title = invariant(
+        form.get("title")?.toString(),
+        "Title is required"
+      );
+      const description = invariant(
+        form.get("description")?.toString(),
+        "Description is required"
+      );
+
+      await db.problemSet.update({
+        where: { sid },
+        data: {
+          title,
+          description,
+        },
+      });
+
+      return null;
+    }
+  }
+
+  throw new Response("I'm a teapot", { status: 418 });
 };
 
 export const meta: MetaFunction = ({ data }: { data: LoaderData }) => ({
   title: `Edit: ${data.problemSet.title} - HITwh OJ`,
 });
 
+function ProblemSetTagItem({ name }: { name: string }) {
+  const fetcher = useFetcher();
+  const isDeleting = fetcher.state !== "idle";
+
+  return (
+    <li style={{ opacity: isDeleting ? 0.25 : 1 }}>
+      {name}{" "}
+      <fetcher.Form method="post" style={{ display: "inline" }}>
+        <input type="hidden" name="tag" value={name} />
+        <button name="_action" value={ActionType.DeleteTag}>
+          删除捏
+        </button>
+      </fetcher.Form>
+    </li>
+  );
+}
+
+function ProblemSetProblemItem({ pid, title }: { pid: number; title: string }) {
+  const fetcher = useFetcher();
+  const isDeleting = fetcher.state !== "idle";
+
+  return (
+    <li style={{ opacity: isDeleting ? 0.25 : 1 }}>
+      {title}{" "}
+      <fetcher.Form method="post" style={{ display: "inline" }}>
+        <input type="hidden" name="pid" value={pid} />
+        <button name="_action" value={ActionType.DeleteProblem}>
+          删除捏
+        </button>
+      </fetcher.Form>
+    </li>
+  );
+}
+
+function TitleEditor({
+  title,
+  description,
+}: {
+  title: string;
+  description: string;
+}) {
+  const fetcher = useFetcher();
+  const isUpdating = fetcher.state !== "idle";
+
+  return (
+    <fetcher.Form method="post">
+      <label htmlFor="title">标题</label>
+      <input
+        id="title"
+        name="title"
+        type="text"
+        defaultValue={title}
+        disabled={isUpdating}
+      />
+      <br />
+      <label htmlFor="description">描述</label>
+      <textarea
+        id="description"
+        name="description"
+        defaultValue={description}
+        disabled={isUpdating}
+      />
+      <button
+        type="submit"
+        disabled={isUpdating}
+        name="_action"
+        value={ActionType.UpdateInfomation}
+      >
+        提交捏
+      </button>
+    </fetcher.Form>
+  );
+}
+
+function ProblemSetTagCreator() {
+  const fetcher = useFetcher();
+  const isCreating = fetcher.state !== "idle";
+
+  return (
+    <fetcher.Form method="post">
+      <input id="tag" name="tag" type="text" disabled={isCreating} required />
+      <button
+        type="submit"
+        name="_action"
+        value={ActionType.CreateTag}
+        disabled={isCreating}
+      >
+        添加捏
+      </button>
+    </fetcher.Form>
+  );
+}
+
+function ProblemSetProblemCreator() {
+  const fetcher = useFetcher();
+  const isCreating = fetcher.state !== "idle";
+
+  return (
+    <fetcher.Form method="post">
+      <input id="pid" name="pid" type="text" disabled={isCreating} required />
+      <button
+        type="submit"
+        name="_action"
+        value={ActionType.CreateProblem}
+        disabled={isCreating}
+      >
+        添加捏
+      </button>
+    </fetcher.Form>
+  );
+}
+
 export default function ProblemSetEdit() {
   const { problemSet } = useLoaderData<LoaderData>();
-  const { state } = useTransition();
 
   return (
     <>
-      <Form method="post">
-        <label htmlFor="title">标题</label>
-        <input
-          id="title"
-          name="title"
-          type="text"
-          disabled={state === "submitting"}
-          defaultValue={problemSet.title}
-        />
-        <br />
-        <label htmlFor="description">描述</label>
-        <textarea
-          id="description"
-          name="description"
-          disabled={state === "submitting"}
-          defaultValue={problemSet.description}
-        />
-        <br />
-        <label htmlFor="tags">标签（按逗号分割）</label>
-        <input
-          id="tags"
-          name="tags"
-          type="text"
-          disabled={state === "submitting"}
-          defaultValue={problemSet.tags.map((tag) => tag.name).join(",")}
-        />
-        <br />
-        <label htmlFor="problems">题目（按逗号分割）</label>
-        <input
-          id="problems"
-          name="problems"
-          type="text"
-          disabled={state === "submitting"}
-          defaultValue={problemSet.problems
-            .map((problem) => problem.pid)
-            .join(",")}
-        />
-        <br />
-        <button type="submit" disabled={state === "submitting"}>
-          提交捏
-        </button>
-      </Form>
+      <h2>标题与简介</h2>
+      <TitleEditor
+        title={problemSet.title}
+        description={problemSet.description}
+      />
+
+      <h2>标签</h2>
+      {problemSet.tags.length ? (
+        <ul>
+          {problemSet.tags.map(({ name }) => (
+            <ProblemSetTagItem name={name} key={name} />
+          ))}
+        </ul>
+      ) : (
+        <div>没有标签捏</div>
+      )}
+      <ProblemSetTagCreator />
+
+      <h2>题目</h2>
+      {problemSet.problems.length ? (
+        <ul>
+          {problemSet.problems.map(({ pid, title }) => (
+            <ProblemSetProblemItem pid={pid} title={title} key={pid} />
+          ))}
+        </ul>
+      ) : (
+        <div>没有题目捏</div>
+      )}
+      <ProblemSetProblemCreator />
     </>
   );
 }
