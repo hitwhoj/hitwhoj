2300e2cbbdee808229ee49c9442bab8aecc156f6
eryi luo <2201110610@stu.hit.edu.cn>
2022-03-14 22:49:36 +0800

    feat: impl contest (base)

--- app/routes/contest/$contestId.tsx
+++ app/routes/contest/$contestId.tsx
@@ -1,6 +1,46 @@
-import { useParams } from "remix";
+import { Contest } from "@prisma/client";
+import { LoaderFunction, json, useLoaderData, MetaFunction } from "remix";
+import { db } from "~/utils/db.server";
+import { invariant } from "~/utils/invariant";
+import { idScheme } from "~/utils/scheme";
+
+type LoaderData = {
+  contest: Contest;
+};
+
+export const loader: LoaderFunction = async ({ params }) => {
+  const cid = invariant(idScheme.safeParse(params.contestId), { status: 404 });
+
+  const contest = await db.contest.findUnique({
+    where: { cid },
+  });
+
+  if (!contest) {
+    throw new Response("Contest not found", { status: 404 });
+  }
+
+  return json({ contest });
+};
+
+export const meta: MetaFunction = ({ data }: { data: LoaderData }) => ({
+  title: data.contest.title,
+  description: data.contest.description,
+});
 
 export default function Contest() {
-  const { contestId } = useParams();
-  return <div> contestId: {contestId} 比赛详情 </div>;
+  const { contest } = useLoaderData<LoaderData>();
+
+  return (
+      <div>
+        <header>
+          <h1>
+          <span style={{ color: "grey", marginRight: "20px" }}>
+            C{contest.cid}
+          </span>
+            {contest.title}
+          </h1>
+        </header>
+        <div style={{ margin: "20px 0" }}>{contest.description}</div>
+      </div>
+  );
 }
--- app/routes/contest/index.tsx
+++ app/routes/contest/index.tsx
@@ -1,3 +1,98 @@
+import { Contest } from "@prisma/client";
+import { LoaderFunction, json, useLoaderData, MetaFunction, Link } from "remix";
+import { db } from "~/utils/db.server";
+
+type LoaderData = {
+  contests: Pick<Contest, "cid" | "title" | "beginTime" | "endTime">[];
+};
+
+export const loader: LoaderFunction = async () => {
+  const contests = await db.contest.findMany({
+    orderBy: [{ cid: "asc" }],
+    take: 20,
+    select: {
+      cid: true,
+      title: true,
+      beginTime: true,
+      endTime: true,
+    },
+  });
+
+  return json({ contests });
+};
+
+export const meta: MetaFunction = () => ({
+  title: "Contest List",
+});
+
+export const contestState = (beginTime: Date, endTime: Date) => {
+  if (endTime.valueOf() && endTime.valueOf() < Date.now()) {
+    return (
+      <span
+        style={{
+          color: "white",
+          backgroundColor: "red",
+          borderRadius: "10px",
+          margin: "0 10px",
+          padding: "0 10px",
+        }}
+      >
+        已结束
+      </span>
+    );
+  } else if (beginTime.valueOf() > Date.now()) {
+    return (
+      <span
+        style={{
+          color: "white",
+          backgroundColor: "blue",
+          borderRadius: "10px",
+          margin: "0 10px",
+          padding: "0 10px",
+        }}
+      >
+        未开始
+      </span>
+    )
+  } else {
+    return (
+      <span
+        style={{
+          color: "white",
+          backgroundColor: "green",
+          borderRadius: "10px",
+          margin: "0 10px",
+          padding: "0 10px",
+        }}
+      >
+        进行中
+      </span>
+    )
+  }
+}
+
 export default function ContestList() {
-  return <div> contestList比赛列表 </div>;
+  const { contests } = useLoaderData<LoaderData>();
+
+  return (
+    <>
+      <h1>Contest List</h1>
+      <ul>
+        {contests.map((contest) => (
+            <li key={contest.cid}>
+              <span
+                  style={{
+                    color: "grey",
+                    marginRight: "10px",
+                  }}
+              >
+                C{contest.cid}
+              </span>
+              <Link to={`/contest/${contest.cid}`}>{contest.title}</Link>
+              {contestState(contest.beginTime, contest.endTime)}
+            </li>
+        ))}
+      </ul>
+    </>
+  );
 }
