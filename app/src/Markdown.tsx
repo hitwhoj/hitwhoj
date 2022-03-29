4307674f132dd42c6fd15f0027c0d803ea76ed2c
swwind <swwind233@gmail.com>
2022-03-29 22:53:33 +0800

    ui: add Highlighter for highlight

--- app/src/Markdown.tsx
+++ app/src/Markdown.tsx
@@ -1,25 +1,20 @@
 import ReactMarkdown from "react-markdown";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import light from "react-syntax-highlighter/dist/esm/styles/prism/prism";
-import night from "react-syntax-highlighter/dist/esm/styles/prism/tomorrow";
+import Highlighter from "./Highlighter";
 
 export function Markdown({ children }: { children: string }) {
   return (
     <ReactMarkdown
       remarkPlugins={[remarkMath]}
       rehypePlugins={[rehypeKatex]}
-      children={children}
       components={{
         code({ node, inline, className, children, ...props }) {
           const match = /language-(\w+)/.exec(className || "");
           return !inline && match ? (
-            <SyntaxHighlighter
+            <Highlighter
               children={String(children).replace(/\n$/, "")}
-              style={Math.random() > 0.5 ? light : night}
               language={match[1]}
-              {...props}
             />
           ) : (
             <code className={className} {...props}>
@@ -28,6 +23,7 @@ export function Markdown({ children }: { children: string }) {
           );
         },
       }}
+      children={children}
     />
   );
 }
