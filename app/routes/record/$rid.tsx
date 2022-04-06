9fef165fecb7fba5fe1adf397dc785384775ac44
swwind <swwind233@gmail.com>
2022-04-06 15:32:31 +0800

    ui: record page

--- app/routes/record/$rid.tsx
+++ app/routes/record/$rid.tsx
@@ -5,6 +5,9 @@ import { s3 } from "~/utils/s3.server";
 import { invariant } from "~/utils/invariant";
 import { idScheme } from "~/utils/scheme";
 import Highlighter from "~/src/Highlighter";
+import { Collapse, Divider, List, Space, Tag } from "@arco-design/web-react";
+import { IconClockCircle, IconStorage } from "@arco-design/web-react/icon";
+import { JudgeResult, SubtaskResult } from "~/utils/types";
 
 type LoaderData = {
   record: Pick<
@@ -54,25 +57,98 @@ export const meta: MetaFunction = ({ data }: { data?: LoaderData }) => ({
   title: `提交记录: ${data?.record.status} - HITwh OJ`,
 });
 
+function ResultMessage({ message }: { message: string }) {
+  return (
+    <span style={{ textOverflow: "ellipsis" }} title={message}>
+      {message}
+    </span>
+  );
+}
+
+function Status({ status }: { status: string }) {
+  if (status === "Accepted") {
+    return <span style={{ color: "rgb(var(--green-6))" }}>Accepted</span>;
+  }
+  if (status.endsWith("Error")) {
+    return <span style={{ color: "rgb(var(--red-6))" }}>{status}</span>;
+  }
+  return <span>{status}</span>;
+}
+
+function TimeTag({ time }: { time: number }) {
+  return <Tag icon={<IconClockCircle />}>{time}ms</Tag>;
+}
+
+function MemoryTag({ memory }: { memory: number }) {
+  return <Tag icon={<IconStorage />}>{memory / 1024}KB</Tag>;
+}
+
+function TimeMemory({ time, memory }: { time: number; memory: number }) {
+  return (
+    <Space>
+      <TimeTag time={time} />
+      <MemoryTag memory={memory} />
+    </Space>
+  );
+}
+
 export default function RecordView() {
   const { record, code } = useLoaderData<LoaderData>();
+  const subtasks: SubtaskResult[] = JSON.parse(record.subtasks);
+
   return (
     <>
-      <h1>{record.status}</h1>
-      <p>Score: {record.score}</p>
-      <p>Time: {record.time} ms</p>
-      <p>Memory: {record.memory} byte</p>
+      <h1>
+        {record.score} {record.status}
+      </h1>
+      <TimeMemory time={record.time} memory={record.memory} />
       {record.message && (
         <>
-          <h2>Compiler Message</h2>
+          <h2>Message</h2>
           <Highlighter language="text" children={record.message} />
         </>
       )}
-      <h2>Result</h2>
-      <Highlighter
-        language="text"
-        children={JSON.stringify(JSON.parse(record.subtasks), null, 2)}
-      />
+      {subtasks.length > 0 && (
+        <>
+          <h2>Result</h2>
+          <Collapse style={{ whiteSpace: "nowrap" }}>
+            {subtasks.map((item, index) => (
+              <Collapse.Item
+                key={index}
+                name={index.toString()}
+                header={
+                  <Space style={{ flex: 1 }}>
+                    <span>Subtask #{index + 1}</span>
+                    <Status status={item.status} />
+                    <ResultMessage message={item.message} />
+                  </Space>
+                }
+                extra={<TimeMemory time={item.time} memory={item.memory} />}
+              >
+                <List
+                  size="small"
+                  dataSource={item.tasks}
+                  render={(task, index) => (
+                    <List.Item
+                      key={index}
+                      children={
+                        <Space>
+                          <span>Task #{index + 1}</span>
+                          <Status status={task.status} />
+                          <ResultMessage message={task.message} />
+                        </Space>
+                      }
+                      extra={
+                        <TimeMemory time={task.time} memory={task.memory} />
+                      }
+                    />
+                  )}
+                />
+              </Collapse.Item>
+            ))}
+          </Collapse>
+        </>
+      )}
       <h2>Source Code</h2>
       <Highlighter language={record.language} children={code} />
     </>
