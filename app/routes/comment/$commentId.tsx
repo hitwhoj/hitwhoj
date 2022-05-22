a703a4b9030314d2bc809f2182ab9a776100a365
luoluo <fhaoze@gmail.com>
2022-05-22 23:55:20 +0800

    feat: comment UI

--- app/routes/comment/$commentId.tsx
+++ app/routes/comment/$commentId.tsx
@@ -1,13 +1,33 @@
 import type { Comment, Reply, User, CommentTag } from "@prisma/client";
 import type { LoaderFunction, MetaFunction } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { useLoaderData, Link } from "@remix-run/react";
 import { invariant } from "~/utils/invariant";
 import { idScheme } from "~/utils/scheme";
 import { db } from "~/utils/server/db.server";
+import {
+  Card,
+  Avatar,
+  Divider,
+  Typography,
+  Space,
+  Tag,
+} from "@arco-design/web-react";
+import {
+  IconExclamationCircle,
+  IconHeart,
+  IconMessage,
+  IconTag,
+} from "@arco-design/web-react/icon";
+import { Markdown } from "~/src/Markdown";
 
 type LoaderData = {
-  comment: Comment & { tags: Pick<CommentTag, "name">[] };
-  replies: (Reply & { creator: Pick<User, "id" | "username" | "avatar"> })[];
+  comment: Comment & {
+    creator: Pick<User, "id" | "username" | "avatar">;
+    tags: Pick<CommentTag, "id" | "name">[];
+    replies: (Reply & {
+      creator: Pick<User, "id" | "username" | "avatar">;
+    })[];
+  };
 };
 
 export const loader: LoaderFunction<LoaderData> = async ({ params }) => {
@@ -19,41 +39,110 @@ export const loader: LoaderFunction<LoaderData> = async ({ params }) => {
     include: {
       creator: {
         select: {
-          nickname: true,
+          id: true,
+          username: true,
+          avatar: true,
         },
       },
       tags: {
         select: {
+          id: true,
           name: true,
         },
+      },
+      replies: {
+        include: {
+          creator: {
+            select: {
+              id: true,
+              username: true,
+              avatar: true,
+            },
+          },
+        },
+        orderBy: {
+          createdAt: "asc",
+        },
+        take: 20,
       },
     },
   });
   if (!comment) {
     throw "comment not found";
   }
-  const replies = await db.reply.findMany({
-    where: { commentId },
-    include: {
-      creator: {
-        select: {
-          id: true,
-          username: true,
-          avatar: true,
-        },
-      },
-    },
-    orderBy: {
-      createdAt: "asc",
-    },
-    take: 20,
-  });
-  return { comment, replies };
+  return { comment };
 };
 
 export const meta: MetaFunction<LoaderData> = ({ data }) => ({
   title: `讨论: ${data?.comment.title} - HITwh OJ`,
 });
+
+function ReplyCard({
+  reply,
+  index,
+}: {
+  reply: Reply & {
+    creator: Pick<User, "id" | "username" | "avatar">;
+  };
+  index: number;
+}) {
+  const author = reply.creator;
+
+  const color = ["orange", "green", "blue", "purple", "magenta", "gold"];
+
+  const Title = () => (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+      }}
+    >
+      <span style={{ display: "flex", alignItems: "center" }}>
+        <Avatar
+          style={{
+            marginRight: 8,
+            backgroundColor: color[Math.floor(Math.random() * color.length)],
+          }}
+          size={32}
+        >
+          {author.avatar ? (
+            <img src={author.avatar} alt="加载失败捏" />
+          ) : (
+            author.username[0]
+          )}
+        </Avatar>
+        <Typography.Text>{author.username}</Typography.Text>
+        <Typography.Text>
+          &emsp;@{reply.createdAt.toLocaleString().slice(0, 16)}
+        </Typography.Text>
+      </span>
+      <Space size={24}>
+        <Space size={8}>
+          <IconTag />
+          {"#" + index}
+        </Space>
+        <Space size={8}>
+          <IconHeart />
+          <Typography.Text>收藏 : 42</Typography.Text>
+        </Space>
+        <Space size={8}>
+          <IconMessage />
+          <Typography.Text>回复 : 63</Typography.Text>
+        </Space>
+        <Space size={8}>
+          <IconExclamationCircle />
+          <Typography.Text>举报 : 0</Typography.Text>
+        </Space>
+      </Space>
+    </div>
+  );
+  return (
+    <Card title={<Title />} style={{ borderColor: "#4E5969" }}>
+      <Markdown>{reply.content}</Markdown>
+    </Card>
+  );
+}
 
 export function ReplyList({
   replies,
@@ -64,30 +153,31 @@ export function ReplyList({
     return <p>暂无回复</p>;
   } else {
     return (
-      <div>
-        {replies.map((reply) => (
-          <p key={reply.id}>
-            <img src={reply.creator.avatar} alt="没有头像捏" />
-            {"   " + reply.creator.username}
-            <h4>{reply.content}</h4>
-            Time: {reply.createdAt}
-          </p>
+      <Space direction="vertical" size={16} style={{ width: "100%" }}>
+        {replies.map((reply, index) => (
+          <ReplyCard key={reply.id} reply={reply} index={index} />
         ))}
-      </div>
+      </Space>
     );
   }
 }
 
 export default function CommentView() {
-  const { comment, replies } = useLoaderData<LoaderData>();
+  const { comment } = useLoaderData<LoaderData>();
   return (
     <>
       <h1>Comment: {comment.title}</h1>
-      Tags:
-      {comment.tags.map((tag) => tag.name).join(", ")}
-      <br />
-      <button>unused report button</button>
-      <ReplyList replies={replies} />
+      <h2>标签</h2>
+      <Space size="medium">
+        <Link to={`/comment/tag/example`} key="example">
+          <Tag>#example</Tag>
+        </Link>
+        <Link to={`/comment/tag/math`} key="math">
+          <Tag>#math</Tag>
+        </Link>
+      </Space>
+      <Divider />
+      <ReplyList replies={comment.replies} />
     </>
   );
 }
--- app/routes/comment/index.tsx
+++ app/routes/comment/index.tsx
@@ -1,19 +1,32 @@
-import type { Comment, User, CommentTag } from "@prisma/client";
+import type { Comment, User } from "@prisma/client";
 import type { LoaderFunction, MetaFunction } from "@remix-run/node";
 import { Link, useLoaderData } from "@remix-run/react";
 import { db } from "~/utils/server/db.server";
+import { Table, Grid, Button, Space } from "@arco-design/web-react";
+import {
+  IconExclamationCircle,
+  // IconExclamationCircleFill,
+  IconHeart,
+  // IconHeartFill,
+  IconMessage,
+  // IconStar,
+  IconStarFill,
+  IconThumbUp,
+  // IconThumbUpFill
+} from "@arco-design/web-react/icon";
 
 type LoaderData = {
-  comments: (Comment & {
-    creator: Pick<User, "id" | "username" | "avatar">;
-    tags: Pick<CommentTag, "name">[];
+  comments: (Pick<Comment, "id" | "title" | "createdAt" | "updatedAt"> & {
+    creator: Pick<User, "id" | "username">;
+    // stars: number;
+    // replies: number;
   })[];
 };
 
 export const loader: LoaderFunction<LoaderData> = async () => {
   const comments = await db.comment.findMany({
     orderBy: {
-      createdAt: "desc",
+      updatedAt: "desc",
     },
     take: 20,
     include: {
@@ -21,14 +34,13 @@ export const loader: LoaderFunction<LoaderData> = async () => {
         select: {
           id: true,
           username: true,
-          avatar: true,
         },
       },
-      tags: {
-        select: {
-          name: true,
-        },
-      },
+      // replies: {
+      //   select: {
+      //     id: true,
+      //   },
+      // }
     },
   });
   return { comments };
@@ -38,43 +50,80 @@ export const meta: MetaFunction = () => ({
   title: "讨论列表 - HITwh OJ",
 });
 
-export function CommentItem({
-  comment,
-}: {
-  comment: Comment & {
-    creator: Pick<User, "id" | "username" | "avatar">;
-    tags: Pick<CommentTag, "name">[];
-  };
-}) {
-  return (
-    <>
-      <img src={comment.creator.avatar} alt="没有头像捏" />
-      {comment.creator.username}
-
-      {"    发布时间:  " + comment.createdAt}
-      {"    最近更新:  " + comment.updatedAt}
-      {"    标签:  " + comment.tags.map((tag) => tag.name).join(", ")}
-      <Link to={`/comment/${comment.id}`}>
-        <h2>{comment.title} ←←点这个进入comment页</h2>
-      </Link>
-    </>
-  );
-}
-
 export function CommentList({
   comments,
 }: {
-  comments: (Comment & {
-    creator: Pick<User, "id" | "username" | "avatar">;
-    tags: Pick<CommentTag, "name">[];
-  })[];
+  comments: LoaderData["comments"];
 }) {
+  type CommentDetails = Pick<
+    Comment,
+    "id" | "title" | "createdAt" | "updatedAt"
+  > & {
+    creator: Pick<User, "id" | "username">;
+  };
+  const tableColumns = [
+    {
+      title: "Forum",
+      render: (_: any, comment: CommentDetails) => (
+        <Link to={`/comment/forum/${comment.id}`}>
+          P123: A + B Problem 公告版
+        </Link>
+      ),
+    },
+    {
+      title: "Title",
+      dataIndex: "title",
+      render: (col: string, comment: CommentDetails) => (
+        <Link
+          to={`/comment/${comment.id}`}
+          // TODO: 写个hover样式qwq
+          style={{}}
+        >
+          {col}
+        </Link>
+      ),
+    },
+    {
+      title: (
+        <Space size={12}>
+          <IconHeart />
+          <IconStarFill />
+          <IconThumbUp />
+          <IconMessage />
+        </Space>
+      ),
+      render: (_: any) => (
+        <Space size={6}>
+          <IconHeart />
+          73 <IconMessage />
+          42
+          <IconExclamationCircle />0
+        </Space>
+      ),
+    },
+    {
+      title: "Author",
+      render: (_: any, comment: CommentDetails) => (
+        <Link to={`/user/${comment.creator.id}`}>
+          {comment.creator.username}
+          {" @"}
+          {comment.createdAt.toLocaleString()}
+        </Link>
+      ),
+    },
+  ];
   return (
-    <div className="comment-list">
-      {comments.map((comment) => (
-        <CommentItem key={comment.id} comment={comment} />
-      ))}
-    </div>
+    <Table
+      columns={tableColumns}
+      data={comments}
+      hover={false}
+      pagination={{
+        total: comments.length,
+        defaultPageSize: 20,
+        showTotal: (total: number) => `Total ${Math.ceil(total / 20)} pages`,
+        showJumper: true,
+      }}
+    />
   );
 }
 
@@ -83,10 +132,17 @@ export default function CommentListIndex() {
 
   return (
     <>
-      <h1>latest comments</h1>
-      <Link to="new">
-        <button>create a comment</button>
-      </Link>
+      <Grid.Row
+        justify="end"
+        align="center"
+        style={{
+          height: "3rem",
+        }}
+      >
+        <Link to="new">
+          <Button type="primary">Create comment</Button>
+        </Link>
+      </Grid.Row>
       <CommentList comments={comments} />
     </>
   );
