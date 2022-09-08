374661fa85f246d6647735e8b7214b3a28fd6f19
Ling_yunchi <2201110428@stu.hit.edu.cn>
2022-09-08 20:10:19 +0800

    fix: calender heatmap data calculate

--- app/routes/user/$userId/statistics.tsx
+++ app/routes/user/$userId/statistics.tsx
@@ -12,6 +12,8 @@ import { formatNumber } from "~/utils/tools";
 import ActivityCalendar from "react-activity-calendar";
 import type { Level } from "react-activity-calendar";
 import ReactTooltip from "react-tooltip";
+import { useContext } from "react";
+import { defaultThemeColor, ThemeContext } from "~/utils/theme";
 
 export async function loader({ request, params }: LoaderArgs) {
   const userId = invariant(idScheme, params.userId, { status: 404 });
@@ -21,7 +23,7 @@ export async function loader({ request, params }: LoaderArgs) {
       ? Permissions.PERM_VIEW_USER_PROFILE_SELF
       : Permissions.PERM_VIEW_USER_PROFILE
   );
-
+  const now = new Date();
   const user = await db.user.findUnique({
     where: { id: userId },
     select: {
@@ -41,6 +43,17 @@ export async function loader({ request, params }: LoaderArgs) {
           createdReplies: true,
         },
       },
+      createdRecords: {
+        select: {
+          submittedAt: true,
+        },
+        where: {
+          submittedAt: {
+            lte: now,
+            gt: new Date(now.getTime() - 365 * 24 * 60 * 60 * 1000),
+          },
+        },
+      },
     },
   });
 
@@ -51,38 +64,75 @@ export async function loader({ request, params }: LoaderArgs) {
   return json({ user });
 }
 
+// generate 5 color from hex fromColor to toColor with 5 steps
+function getLevelColors(fromColor: string, toColor: string) {
+  const fromRGB = hexToRGB(fromColor);
+  const toRGB = hexToRGB(toColor);
+  const levelColors: string[] = [];
+  for (let i = 0; i < 5; i++) {
+    const color = `rgb(${fromRGB[0] + (toRGB[0] - fromRGB[0]) * (i / 4)}, ${
+      fromRGB[1] + (toRGB[1] - fromRGB[1]) * (i / 4)
+    }, ${fromRGB[2] + (toRGB[2] - fromRGB[2]) * (i / 4)})`;
+    levelColors.push(color);
+  }
+  return levelColors;
+}
+
+// Hex color to RGB color
+function hexToRGB(color: string) {
+  const r = parseInt(color.slice(1, 3), 16);
+  const g = parseInt(color.slice(3, 5), 16);
+  const b = parseInt(color.slice(5, 7), 16);
+  return [r, g, b];
+}
+
 export default function UserStatistics() {
   const { user } = useLoaderData<typeof loader>();
+  const theme = useContext(ThemeContext);
+  const fromColor = defaultThemeColor[theme].base200;
+  const toColor = defaultThemeColor[theme].primary;
+  const [level0, level1, level2, level3, level4] = getLevelColors(
+    fromColor,
+    toColor
+  );
+  const now = new Date();
 
   // FIXME 摆烂了，这个页面就跟 profile 页面合并了吧
   // 丢给 @lingyunchi 处理吧
-  const now = new Date();
+
+  // prettier-ignore
+  const ActivityCalendarData = Object.entries(user.createdRecords.reduce((cur, record) => {
+    const date = new Date(record.submittedAt).toISOString().slice(0, 10);
+    if (cur[date]) cur[date]++;
+    else cur[date] = 1;
+    return cur;
+  }, {
+    [now.toISOString().slice(0, 10)]: 0,
+    [new Date(now.getTime() - 365 * 24 * 60 * 60 * 1000).toISOString().slice(0, 10)]: 0,
+  } as Record<string, number>)).map(([date, count]) => {
+    const level = Math.min(Math.floor(count / 2), 4) as Level;
+    return { date, count, level };
+  }).sort((a, b) => {
+    return a.date > b.date ? 1 : -1;
+  });
 
   return (
     <>
       <div className="stats w-full">
         <div className="stat place-items-center">
           <ActivityCalendar
-            data={[
-              ...Array.from({ length: 365 }).map((_, i) => {
-                const date = new Date(now.getTime() + i * 24 * 60 * 60 * 1000);
-                return {
-                  count: Math.floor(Math.random() * 10),
-                  date: date.toISOString().slice(0, 10),
-                  level: Math.floor(Math.random() * 5) as Level,
-                };
-              }),
-            ]}
+            data={ActivityCalendarData}
             labels={{
-              tooltip: "<strong>{{count}} contributions</strong> on {{date}}",
-              totalCount: "{{count}} contributions in {{year}}",
+              tooltip:
+                "<span style='font-weight: bold;' color='white'>{{count}} submitions</span> on {{date}}",
+              totalCount: "{{count}} submitions in {{year}}",
             }}
             theme={{
-              level0: "#ebedf0",
-              level1: "#c6e48b",
-              level2: "#7bc96f",
-              level3: "#239a3b",
-              level4: "#196127",
+              level0: level0,
+              level1: level1,
+              level2: level2,
+              level3: level3,
+              level4: level4,
             }}
           >
             <ReactTooltip html />
