33b0bfb0db2860c9d44a340dc2c6de1563022e66
chino <839203521@qq.com>
2022-09-29 21:37:15 +0800

    feat: problem search

--- app/routes/problem/index.tsx
+++ app/routes/problem/index.tsx
@@ -1,15 +1,15 @@
 import type { LoaderArgs, MetaFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import { Link, useLoaderData, useNavigate } from "@remix-run/react";
+import { Link, useLoaderData, useNavigate, useParams } from "@remix-run/react";
 import { selectProblemListData } from "~/utils/db/problem";
 import { db } from "~/utils/server/db.server";
 import { ProblemLink } from "~/src/problem/ProblemLink";
 import { findRequestUser } from "~/utils/permission";
 import { Permissions } from "~/utils/permission/permission";
 import { HiOutlinePlus } from "react-icons/hi";
-import { pageScheme } from "~/utils/scheme";
+import { descriptionScheme, pageScheme } from "~/utils/scheme";
 import { invariant } from "~/utils/invariant";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { Pagination } from "~/src/Pagination";
 
 const pageSize = 15;
@@ -26,23 +26,33 @@ export async function loader({ request }: LoaderArgs) {
 
   const url = new URL(request.url);
   const page = invariant(pageScheme, url.searchParams.get("page") || "1");
+  const keyword = invariant(
+    descriptionScheme,
+    url.searchParams.get("keyword") || ""
+  );
   const totalProblems = await db.problem.count({
-    where: viewAll
-      ? { team: null }
-      : viewPublic
-      ? { team: null, private: false }
-      : { id: -1 },
+    where: Object.assign(
+      viewAll
+        ? { team: null }
+        : viewPublic
+        ? { team: null, private: false }
+        : { id: -1 },
+      { title: { contains: keyword } }
+    ),
   });
   if (totalProblems && page > Math.ceil(totalProblems / pageSize)) {
     throw new Response("Page is out of range", { status: 404 });
   }
 
   const problems = await db.problem.findMany({
-    where: viewAll
-      ? { team: null }
-      : viewPublic
-      ? { team: null, private: false }
-      : { id: -1 },
+    where: Object.assign(
+      viewAll
+        ? { team: null }
+        : viewPublic
+        ? { team: null, private: false }
+        : { id: -1 },
+      { title: { contains: keyword } }
+    ),
     orderBy: [{ id: "asc" }],
     select: {
       ...selectProblemListData,
@@ -74,6 +84,10 @@ export default function ProblemIndex() {
     () => Math.ceil(totalProblems / pageSize),
     [totalProblems]
   );
+  const [problemIdInput, setProblemIdInput] = useState("");
+  const [problemNameInput, setProblemNameInput] = useState(
+    useParams().keyword || ""
+  );
 
   return (
     <>
@@ -86,6 +100,51 @@ export default function ProblemIndex() {
           </Link>
         )}
       </h1>
+
+      <div className="flex flex-row justify-between flex-wrap">
+        <div className="space-x-2 mt-1">
+          <input
+            type="text"
+            className="input input-bordered"
+            placeholder="题目 ID"
+            value={problemIdInput}
+            onChange={(e) => setProblemIdInput(e.target.value)}
+            onKeyDown={(e) =>
+              e.key === "Enter" && navigate(`${problemIdInput}`)
+            }
+          />
+          <button
+            className="btn btn-primary"
+            onClick={() => navigate(`${problemIdInput}`)}
+          >
+            前往
+          </button>
+        </div>
+        <div className="space-x-2 mt-1">
+          <span>
+            <input
+              type="text"
+              className="input input-bordered"
+              placeholder="标题"
+              value={problemNameInput}
+              onChange={(e) => setProblemNameInput(e.target.value)}
+              onKeyDown={(e) =>
+                e.key === "Enter" &&
+                navigate(`?page=1&keyword=${problemNameInput}`)
+              }
+            />
+          </span>
+
+          <button
+            className="btn btn-primary"
+            onClick={() =>
+              navigate(`?page=${currentPage}&keyword=${problemNameInput}`)
+            }
+          >
+            搜索
+          </button>
+        </div>
+      </div>
 
       <table className="table table-compact w-full not-prose">
         <thead>
@@ -110,7 +169,9 @@ export default function ProblemIndex() {
       <Pagination
         totalPages={totalPages}
         currentPage={currentPage}
-        onPageChange={(page) => navigate(`?page=${page}`)}
+        onPageChange={(page) =>
+          navigate(`?page=${page}&keyword=${problemNameInput}`)
+        }
       />
     </>
   );
