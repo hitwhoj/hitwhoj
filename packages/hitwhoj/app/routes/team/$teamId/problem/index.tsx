ad71f3586bf51e1672fb270b3f9f2edb07e5477c
jcyyy3 <2022212018@stu.hit.edu.cn>
2023-09-23 00:15:51 +0800

    Merge branch '27-oj-2' of https://git.hit.edu.cn/hitwhoj/hitwhoj into 27-oj-2
    # Conflicts:
    #	packages/hitwhoj/app/routes/problemset/$problemSetId/edit.tsx
    #	packages/hitwhoj/app/src/problem/ProblemEditorCreator.tsx

---
+++ packages/hitwhoj/app/routes/team/$teamId/problem/index.tsx
@@ -0,0 +1,119 @@
+import { useComputed } from "@preact/signals-react";
+import { type MetaFunction, type LoaderArgs, json } from "@remix-run/node";
+import { Link } from "@remix-run/react";
+import { HiOutlinePlus } from "react-icons/hi";
+import { Pagination } from "~/src/Pagination";
+import { ProblemLink } from "~/src/problem/ProblemLink";
+import { selectProblemListData } from "~/utils/db/problem";
+import { useSignalLoaderData } from "~/utils/hooks";
+import { invariant } from "~/utils/invariant";
+import { findRequestUser } from "~/utils/permission";
+import { Permissions } from "~/utils/permission/permission";
+import { idScheme, pageScheme } from "~/utils/scheme";
+import { db } from "~/utils/server/db.server";
+
+const PAGE_SIZE = 15;
+export async function loader({ request, params }: LoaderArgs) {
+  const self = await findRequestUser(request);
+  const teamId = await invariant(idScheme, params.teamId, { status: 404 });
+  const [viewAll, viewPublic, hasCreatePerm] = await self
+    .team(null)
+    .hasPermission(
+      Permissions.PERM_VIEW_PROBLEM,
+      Permissions.PERM_VIEW_PROBLEM_PUBLIC,
+      Permissions.PERM_CREATE_PROBLEM
+    );
+  const url = new URL(request.url);
+  const page = invariant(pageScheme, url.searchParams.get("page") || "1");
+  const totalProblems = await db.problem.count({
+    where: viewAll
+      ? { teamId: teamId }
+      : viewPublic
+      ? { teamId: teamId, private: false }
+      : { id: -1 },
+  });
+  if (totalProblems && page > Math.ceil(totalProblems / PAGE_SIZE)) {
+    throw new Response("Page is out of range", { status: 404 });
+  }
+  const problems = await db.problem.findMany({
+    where: viewAll
+      ? { teamId: teamId }
+      : viewPublic
+      ? { teamId: teamId, private: false }
+      : { id: -1 },
+    orderBy: [{ id: "asc" }],
+    select: {
+      ...selectProblemListData,
+      _count: {
+        select: {
+          relatedRecords: true,
+        },
+      },
+    },
+    skip: (page - 1) * PAGE_SIZE,
+    take: PAGE_SIZE,
+  });
+
+  return json({ problems, hasCreatePerm, totalProblems, currentPage: page });
+}
+
+export const meta: MetaFunction = () => ({
+  title: "团队题目 - HITwh OJ",
+});
+
+export default function ProblemIndex() {
+  const loaderData = useSignalLoaderData<typeof loader>();
+  const problems = useComputed(() => loaderData.value.problems);
+  const hasCreatePerm = useComputed(() => loaderData.value.hasCreatePerm);
+  const totalProblems = useComputed(() => loaderData.value.totalProblems);
+  const currentPage = useComputed(() => loaderData.value.currentPage);
+
+  const totalPages = useComputed(() =>
+    Math.ceil(totalProblems.value / PAGE_SIZE)
+  );
+
+  return (
+    <>
+      <h2 className="flex items-center justify-between">
+        <span>题目列表</span>
+        {hasCreatePerm.value && (
+          <Link to="new" className="btn btn-primary gap-2">
+            <HiOutlinePlus className="h-4 w-4" />
+            <span>新建题目</span>
+          </Link>
+        )}
+      </h2>
+
+      <table className="not-prose table-compact table w-full">
+        <thead>
+          <tr>
+            <th className="w-16" />
+            <th>题目</th>
+            <th>提交</th>
+          </tr>
+        </thead>
+        <tbody>
+          {problems.value.map((problem, index) => (
+            <tr key={problem.id}>
+              <th className="text-center">
+                {index + 1 + (Number(currentPage) - 1) * PAGE_SIZE}
+              </th>
+              <td>
+                <ProblemLink problem={problem} />
+              </td>
+              <td>{problem._count.relatedRecords}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <Pagination
+        action="/problem"
+        totalPages={totalPages.value}
+        currentPage={currentPage.value}
+      />
+    </>
+  );
+}
+
+export { ErrorBoundary } from "~/src/ErrorBoundary";
+export { CatchBoundary } from "~/src/CatchBoundary";
---
+++ packages/hitwhoj/app/routes/team/$teamId/problem/new.tsx
@@ -0,0 +1,80 @@
+import { redirect, type LoaderArgs, type ActionArgs } from "@remix-run/node";
+import { Form } from "@remix-run/react";
+import { useSignalTransition } from "~/utils/hooks";
+import { invariant } from "~/utils/invariant";
+import { findRequestUser } from "~/utils/permission";
+import { Permissions } from "~/utils/permission/permission";
+import { Privileges } from "~/utils/permission/privilege";
+import { idScheme, titleScheme } from "~/utils/scheme";
+import { db } from "~/utils/server/db.server";
+
+export async function loader({ request, params }: LoaderArgs) {
+  const teamId = await invariant(idScheme, params.teamId, { status: 404 });
+  const self = await findRequestUser(request);
+  await self.checkPrivilege(Privileges.PRIV_OPERATE);
+  await self.team(teamId).checkPermission(Permissions.PERM_CREATE_PROBLEM);
+
+  for (var i = 0; i < 100; ++i) {
+    console.log("Hello!");
+  }
+  return null;
+}
+
+export async function action({ request, params }: ActionArgs) {
+  const self = await findRequestUser(request);
+  const teamId = await invariant(idScheme, params.teamId, { status: 404 });
+  await self.checkPrivilege(Privileges.PRIV_OPERATE);
+  await self.team(teamId).checkPermission(Permissions.PERM_CREATE_PROBLEM);
+  const form = await request.formData();
+  const title = invariant(titleScheme, form.get("title"));
+
+  for (var i = 0; i < 100; ++i) {
+    console.log("teamId: ", teamId);
+  }
+  const { id } = await db.problem.create({
+    data: {
+      title: title,
+      teamId: teamId,
+    },
+  });
+
+  return redirect(`/problem/${id}/edit`);
+}
+
+export default function ProblemNew() {
+  const transition = useSignalTransition();
+
+  return (
+    <>
+      <h2>新建题目</h2>
+
+      <Form method="post" className="form-control w-full max-w-xs gap-4">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">题目名称</span>
+          </label>
+          <input
+            className="input input-bordered"
+            type="text"
+            name="title"
+            required
+            disabled={transition.isRunning}
+          />
+        </div>
+
+        <div className="form-control">
+          <button
+            className="btn btn-primary"
+            type="submit"
+            disabled={transition.isRunning}
+          >
+            创建题目
+          </button>
+        </div>
+      </Form>
+    </>
+  );
+}
+
+export { ErrorBoundary } from "~/src/ErrorBoundary";
+export { CatchBoundary } from "~/src/CatchBoundary";
